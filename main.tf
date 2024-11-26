561cf554fc961e7af0f0912e958c25c2d267750d
praveewin-create <gpgp0883@gmail.com>
2024-11-25 19:19:31 -0700

    updated the permission

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "test_res" {
   filename = "/c/tf-workspace/first-git/tf-workspace/tf-workspace/test.txt"
   content = "i love united states"
-  directory_permission = "0755"
-  file_permission = "775"
+  directory_permission = "0777"
+  file_permission = "777"
 }
